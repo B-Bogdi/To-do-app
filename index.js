35d25c0fdd902910c45259df828a25ec960d8a7c
B-Invisible <114253054+B-Invisible@users.noreply.github.com>
2022-11-23 10:05:35 -0800

    Add files via upload
    Signed-off-by: B-Invisible <114253054+B-Invisible@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const input = document.getElementById("inpuT")
 const addTasks = document.getElementById("addtask")
 const clearTasks = document.getElementById("cleartasks")
 
+const tasks = document.getElementById("tasks");
+
 addTasks.addEventListener("click", addtasks)
 clearTasks.addEventListener("click", cleartasks)
+
+tasks.addEventListener("click", handleTaskClick);
 
 if (tasks.innerHTML == ""){
     provide.innerHTML = '<p  class="knoting">You do not have tasks!:)</p>'
@@ -12,7 +16,7 @@ if (tasks.innerHTML == ""){
 function addtasks() {
     if (input.value != "") {
         provide.innerHTML = ""
-        tasks.innerHTML += '<input type="checkbox" id="taskCheck" ><p id="taskslist" contenteditable="true"> ' + input.value + ' </p>'
+        tasks.innerHTML += '<p id="taskslist" > - ' + input.value + ' </p>'
         tasks.innerHTML += '<br>'
         input.value = ""
     } else {
@@ -23,4 +27,13 @@ function addtasks() {
 function cleartasks() {
     tasks.innerHTML = "";
     provide.innerHTML = '<p class="knoting">You do not have tasks!:)</p>'
+}
+
+function handleTaskClick(event) {
+    const style = event.target.style;
+    if (!style.textDecoration) {
+        style.textDecoration = "line-through";
+    } else {
+        style.textDecoration = "";
+    }
 }
